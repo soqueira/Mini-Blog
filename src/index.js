e117c88f05d8ae3fc986e662533e266e20ba5bb2
soqueira <vitorsiqueria@gmail.com>
2020-01-22 15:47:28 -0300

    Add: connect redux to firebase

--- src/index.js
+++ src/index.js
@@ -2,23 +2,40 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./store/reducers/rootReducer";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
+import firebase, {createFirestoreInstance} from "firebase";
+import { reduxFirestore, getFirestore } from "redux-firestore";
+import {getFirebase, ReactReduxFirebaseProvider } from "react-redux-firebase";
+import fbConfig from "./config/fbConfig";
 
 import "./index.css";
 
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css";
 
-const store = createStore(rootReducer, applyMiddleware(thunk) );
-
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirestore, getFirebase })),
+    reduxFirestore(firebase, fbConfig)
+  )
+)
+const rrfProps = {
+  firebase,
+  config: fbConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance
+};
 M.AutoInit();
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
+   <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
   </Provider>,
   document.getElementById("root")
 );
