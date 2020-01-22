0593d343806fcc2b90db3344e0d623629f25fd38
soqueira <vitorsiqueria@gmail.com>
2020-01-21 22:27:45 -0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+M.AutoInit();
+
+ReactDOM.render(<App />, document.getElementById("root"));
