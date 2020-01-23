d1081886c20d624c568baa64da00c056805086c2
soqueira <vitorsiqueria@gmail.com>
2020-01-22 23:03:55 -0300

    Add: user logOut

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -1,20 +1,32 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { connect } from "react-redux";
+import { signOut } from "../../store/actions/authActions";
 
-export default () => {
+const SignedInLinks = props => {
   return (
     <ul className="right">
       <li>
         <NavLink to="/createproject">New Project</NavLink>
       </li>
-      <li>
+      <li onClick={() => props.signOut()}>
         <NavLink to="/">LogOut</NavLink>
       </li>
       <li>
-        <NavLink to="/" className="btn waves-effect waves-light btn-floating pink lighten-1">
+        <NavLink
+          to="/"
+          className="btn waves-effect waves-light btn-floating pink lighten-1"
+        >
           NN
         </NavLink>
       </li>
     </ul>
   );
 };
+const mapDispatchToProps = dispatch => {
+  return {
+    signOut: () => dispatch(signOut())
+  };
+};
+
+export default connect(null, mapDispatchToProps)(SignedInLinks);
--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -9,12 +9,15 @@ const authReducer = (state = initState, action) => {
         authError: "login failed"
       };
     case "LOGIN_SUCCESS":
-      console.log("login succ");
+      console.log("login success");
       return {
         ...state,
         authError: null
       };
-    default: 
+    case "SIGNOUT_SUCCESS":
+      console.log("signout success");
+      return state;
+    default:
       return state;
   }
 };
