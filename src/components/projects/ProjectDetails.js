9b67a968b1c071eedf41b7eafe8c241b48d0f651
soqueira <vitorsiqueria@gmail.com>
2020-01-23 10:52:45 -0300

    Change: momentjs post data

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import { compose } from "redux";
+import moment from "moment";
 
 const ProjectDetails = props => {
   const { project } = props;
@@ -15,7 +16,7 @@ const ProjectDetails = props => {
         </div>
         <div className="card-action lighten-4 grey-text">
           <div>posted by {project.authorName}</div>
-          <div>date</div>
+          <div>{moment(project.createdAt.toDate()).calendar()}</div>
         </div>
       </div>
     </div>
--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,4 +1,5 @@
 import React from "react";
+import moment from "moment";
 
 export default ({project}) => {
   return (
@@ -6,7 +7,7 @@ export default ({project}) => {
       <div className="card-content grey-text text-darken-3">
         <span className="card-title">{project.title}</span>
         <p>Posted by {project.authorName}</p>
-        <p className="grey-text">data</p>
+        <p className="grey-text">{moment(project.createdAt.toDate()).calendar()}</p>
       </div>
     </div>
   );
